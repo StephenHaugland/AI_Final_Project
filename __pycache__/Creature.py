bad7125d53ea17d6de28901599efec595ca5631b
Stephen <haugland98@gmail.com>
2020-04-30 21:27:34 -0700

    Creature Class

---
+++ __pycache__/Creature.py
@@ -0,0 +1,10 @@
+# Stephen Haugland and Shane Snediker
+# This file contains the creature class 
+
+
+# Test Class
+class Creature:
+    DNA = [1,2,3,4]
+
+    def hello(self):
+        print("hello")
