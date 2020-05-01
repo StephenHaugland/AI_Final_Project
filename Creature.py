8535ea9af4aa58fe85a6b46a154bc0b178098855
Stephen <haugland98@gmail.com>
2020-04-30 22:22:49 -0700

    Random Constructor

--- Creature.py
+++ Creature.py
@@ -1,10 +1,34 @@
 # Stephen Haugland and Shane Snediker
+# Artificial Intelligence Spring 2020
 # This file contains the creature class 
 
+import random # used for randomly seeding DNA
 
-# Test Class
+
+# Creature Class
+# This class contains all data pertaining to the agents that will be navigating our maze
 class Creature:
-    DNA = [1,2,3,4]
+    currentOrientation = None # Specifies which direction the creature is facing
+    currentLocation = [0,0] # Start each creature at the starting point of the maze
+    DNA = [None] * 50 # This will hold the list of actions that the creature will take
 
-    def hello(self):
-        print("hello")
+
+    # initial randomized constructor, to be used to create first generation
+    def __init__(self):
+        self.currentOrientation = 'E' # Start all creatures facing towards the right
+        # generate 50 random actions/movements to seed the first generation
+        for x in range(50):
+            self.DNA[x] = random.choice(['L', 'F', 'R'])
+
+    # Test function to display DNA
+    def printDNA(self):
+        print(self.DNA)
+
+    # constructor used in crossover
+    #def __init__(self, mom, dad):
+        
+
+
+test = Creature()
+test.printDNA()
+
